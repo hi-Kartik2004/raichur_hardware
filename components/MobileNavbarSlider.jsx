aef5b5c9fc1687493d7fdb224e608e385146b577
Kartikeya <kudlu2004@gmail.com>
2024-05-07 16:02:45 +0530

    Added navlinks in the mobile slider

--- components/MobileNavbarSlider.jsx
+++ components/MobileNavbarSlider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Slider } from "./ui/slider";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -31,9 +32,46 @@ function MobileNavbarSlider() {
             <div className="mt-4">
               <p className="underline underline-offset-8">Links</p>
             </div>
-            <div className="">
-              <Link href="/">Home</Link>
+            <div className="flex justify-start items-center">
+              <Link href="/">
+                <SheetClose>Home</SheetClose>
+              </Link>
             </div>
+
+            <div className="flex justify-start items-center">
+              <Link href="/faq">
+                <SheetClose>FAQ's</SheetClose>
+              </Link>
+            </div>
+
+            <div className="flex justify-start items-center">
+              <Link href="/contact">
+                <SheetClose>Contact</SheetClose>
+              </Link>
+            </div>
+
+            <div className="mt-4">
+              <p className="underline underline-offset-8">Categories</p>
+            </div>
+
+            <div className="flex justify-start items-center">
+              <Link href="/category/1">
+                <SheetClose>Category 1</SheetClose>
+              </Link>
+            </div>
+
+            <div className="flex justify-start items-center">
+              <Link href="/category/2">
+                <SheetClose>Category 2</SheetClose>
+              </Link>
+            </div>
+
+            <div className="flex justify-start items-center">
+              <Link href="/category/3">
+                <SheetClose>Category 3</SheetClose>
+              </Link>
+            </div>
+
             {/* <div className="flex w-full">
               <SignInButton className="flex w-full" />
             </div> */}
