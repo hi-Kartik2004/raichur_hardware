86b0a1fe213e59019511baeb7ca9b369527eaa61
Kartikeya <kudlu2004@gmail.com>
2024-05-28 23:11:17 +0530

    Changed top padding in gallery section

--- app/(user)/(public)/gallery/page.js
+++ app/(user)/(public)/gallery/page.js
@@ -3,7 +3,7 @@ import { GalleryPagev0 } from "@/components/component/gallery-pagev0";
 // Page component to use GalleryPagev0
 function Page() {
   return (
-    <div className="pt-28">
+    <div className="pt-16 lg:pt-28">
       <GalleryPagev0 />
     </div>
   );
