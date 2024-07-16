ca51786ce8d38fce4a4c1cf0115de090223a838b
Kartikeya <kudlu2004@gmail.com>
2024-07-16 23:27:19 +0530

    Added new meta data

--- app/(user)/page.js
+++ app/(user)/page.js
@@ -58,6 +58,52 @@ export default async function Home() {
   return (
     <>
       <main>
+        <h2 className="hidden">
+          Raichur Hardware Store, PVC in raichur, CPVC in raichur, SWR in
+          raichur & ASTM Pipes in raichur, Drainage & Sewage Pipes in raichur,
+          CPVC fittings in raichur, Agricultural Fittings in raichur, PVC
+          Self-Fit Pipes in raichur, PVC Plain Pipes in raichur, PVC Self-Fit
+          (slotted) Pipes in raichur, PVC U Column Pipes Coupler in raichur,
+          ASTM Plumbing Pipes in raichur, CPVC Pipes in raichur, SWR Pipes in
+          raichur, Self-Fit Pipes in raichur, Ring-Fit Pipes in raichur, Coupler
+          & Union in raichur, Elbows in raichur, Tee(s) in raichur, Reducer &
+          Reducing Bush in raichur, End Cap & THRD End Plug in raichur, Threaded
+          Adapters in raichur (MTA & FTA), Bends in raichur, Clip & Clamps in
+          raichur, Tank & Hex Nipple in raichur, UPVC Valves in raichur,
+          Converter Couplers in raichur, Reducer in raichur, End Cap in raichur,
+          Bends & Transition Bush in raichur, Tank Nipple in raichur, CPVC Ball
+          Valve in raichur, Flange with Gasket in raichur, Mixer in raichur,
+          CPVC Fittings in raichur (SCH 40), CPVC Fittings in raichur (SCH 80),
+          Elbow 90° in raichur (Plain and THRD), Elbow 45° in raichur(Plain),
+          End Cap (Plain and THRD), Tail Piece in raichur, Butterfly Valve in
+          raichur, Service Saddle in raichur (THRD), Reducing Bush in raichur,
+          Single Y in raichur (Reducing & non-reducing), Bend in raichur (45° &
+          90°), Repairing Coupler in raichur (R/R), Single Y & Double Y in
+          raichur, Bend in raichur (Double Socket), Tee in raichur (Double
+          Socket), Gully Trap in raichur, Multi-Floor Trap in raichur, Nahani
+          Trap in raichur, P-Trap & S-Trap in raichur, Vent Cowl in raichur, WC
+          Connector in raichur, Socket Plug & Door Cap in raichur, Drip
+          Irrigation in raichur, Sprinkle Irrigation in raichur, Subsurface
+          Drip, PVC Ball Valves, Faucets, Shower, Thermostatic Mixers in
+          raichur, Shower panel in raichur, Water heaters in raichur, Sanitary
+          ware in raichur, Wellness in raichur, Shower enclosures in raichur,
+          Flushing systems in raichur, Drains in raichur, Bath Accessories in
+          raichur, Digital locks in raichur, Mortise locks in raichur, Rim locks
+          in raichur, Cylindrical locks in raichur, Padlocks, Furniture locks in
+          raichur, Home décor handles in raichur, Door controls in raichur, Door
+          fittings in raichur, Wardrobe fittings in raichur, Bed fittings in
+          raichur, Furniture fittings in raichur, Kitchen fittings in raichur,
+          Luxury Kitchen Sinks in raichur, Quartz Kitchen Sinks in raichur,
+          Stainless steel sinks in raichur, Open Well Pumps in raichur,
+          Submersible Pumps in raichur, Monoblock Pumps in raichur, Jet Pumps in
+          raichur, Sewage Pumps in raichur, Electric Induction Motors in
+          raichur, CMBE And CMBE Twin in raichur, CM Single Booster in raichur,
+          SCALA1 and SCALA2 booster in raichur, Shower and other Boosters in
+          raichur, Overhead Water Tanks in raichur, Ball Valve in raichur, Gate
+          Valve in raichur, Sluice Valve in raichur, Horizontal Check Valve in
+          raichur, Vertical Check Valve in raichur, Cast Iron Manhole covers in
+          raichur.
+        </h2>
         <HeroSection />
         <Separator />
         <FeaturedLogos />
